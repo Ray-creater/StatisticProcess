618b510222538adb8aa9e1544c546aab0f56b64d
Raycl <493757973@qq.com>
2020-11-20 18:26:27 +0800

    Improve aaa.py

--- GUI/aaa.py
+++ GUI/aaa.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore,QtGui
 from PyQt5.QtGui import QIcon
 import qtawesome
 
@@ -12,6 +12,7 @@ class mywindow(QtWidgets.QMainWindow):
     def initUI(self):
         self.layout()
         self.addLeftWidget()
+        self.addRightWidget()
         self.show()
         
 
@@ -28,11 +29,11 @@ class mywindow(QtWidgets.QMainWindow):
 
         self.rightWidget=QtWidgets.QWidget()
         self.rightWidget.setObjectName('right widget')
-        self.rightLayout=QtWidgets.QGridLayout()
+        self.rightLayout=QtWidgets.QVBoxLayout()
         self.rightWidget.setLayout(self.rightLayout)
 
         self.mainLayout.addWidget(self.leftWidget,0,0,12,2)
-        self.mainLayout.addWidget(self.rightWidget,0,2,12,10)
+        self.mainLayout.addWidget(self.rightWidget,0,6,12,3)
         self.setCentralWidget(self.mainWidget)
         self.setWindowTitle('Teaching Tool')
 
@@ -47,27 +48,48 @@ class mywindow(QtWidgets.QMainWindow):
         self.chooseLabel=QtWidgets.QLabel('Choose component')
         self.chooseBeam=QtWidgets.QRadioButton('Beam')
         self.chooseColumns=QtWidgets.QRadioButton('Columns')
-        self.chooseBeam.setCheckable(True)
+        self.chooseBeam.setChecked(True)
+        self.chooseColumns.setChecked(False)
+        self.verifiedButton=QtWidgets.QPushButton('ok')
+
+        self.verifiedButton.clicked.connect(self.showFailuremode)
         
         
 
-        self.leftLayout.addWidget(self.leftClose,0,0,1,1)
-        self.leftLayout.addWidget(self.leftVisit,0,1,1,1)
-        self.leftLayout.addWidget(self.leftMini,0,2,1,1)
+        # self.leftLayout.addWidget(self.leftClose,0,0,1,1)
+        # self.leftLayout.addWidget(self.leftVisit,0,1,1,1)
+        # self.leftLayout.addWidget(self.leftMini,0,2,1,1)
         self.leftLayout.addWidget(self.chooseBeam,3,0,1,1)
-        self.leftLayout.addWidget(self.chooseColumns,3,1,1,2)
+        self.leftLayout.addWidget(self.chooseColumns,3,1,1,1)
+        self.leftLayout.addWidget(self.verifiedButton,3,2,1,1)
+        # self.leftLayout.addWidget(self.flexure,6,0,1,3)
+        # self.leftLayout.addWidget(self.compression,7,0,1,3)
+        # self.leftLayout.addWidget(self.tension,8,0,1,3)
+
+
+    def showFailuremode(self):
+        if self.chooseColumns.isChecked():
+            self.flexure.deleteLater()
+            self.columnLayout()
+        else:
+            self.beamLayout()
+    
+    def beamLayout(self):
+        self.flexure=QtWidgets.QPushButton("Flexure")
         self.leftLayout.addWidget(self.flexure,6,0,1,3)
         self.leftLayout.addWidget(self.compression,7,0,1,3)
         self.leftLayout.addWidget(self.tension,8,0,1,3)
-    
-    def beamLayout(self):
-        pass
 
-    
 
     def columnLayout(self):
-        pass
+        self.leftLayout.addWidget(self.compression,7,0,1,3)
+        self.leftLayout.addWidget(self.tension,8,0,1,3)
 
+    def addRightWidget(self):
+        conceptpix=QtGui.QPixmap('1.png')
+        self.conceptLabel=QtWidgets.QLabel('Conceplt\' picture')
+        self.conceptLabel.setPixmap(conceptpix)
+        self.rightLayout.addWidget(self.conceptLabel)
         
         
 
