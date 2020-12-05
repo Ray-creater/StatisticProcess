98ccca81e6ace6f9bff72487540e1b40bd71ef04
Raycl <493757973@qq.com>
2020-12-06 00:02:53 +0800

    Adjust the layout of windows

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,8 +1,9 @@
 import sys 
-from PyQt5 import QtCore,QtWidgets,QtGui
-import hysteresis as hy
+from PyQt5.QtGui import * 
+from PyQt5.QtWidgets import *
+import Component as cp
 
-class Mywindow(QtWidgets.QWidget):
+class Mywindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -10,80 +11,72 @@ class Mywindow(QtWidgets.QWidget):
 
 
     def initUI(self):
+        self.mainWidget=QWidget()
+        self.centralLayout=QVBoxLayout()
+        self.mainWidget.setLayout(self.centralLayout)
+        self.setCentralWidget(self.mainWidget)
 
-        datFilePathButton=QtWidgets.QPushButton("Choose .dat File Path",self)
-        logFilePathButton=QtWidgets.QPushButton('Choose .log File Path',self)
-        readButton=QtWidgets.QPushButton('Read Data',self)
-        plotButton=QtWidgets.QPushButton('plot',self)
+        self.chooseLayout=QFormLayout()
+        self.centralLayout.addLayout(self.chooseLayout)
+
+        self.datFilePathButton=QPushButton("Choose .dat File Path")
+        self.lableDatFile=QLabel('None')
+        self.chooseLayout.addRow(self.datFilePathButton,self.lableDatFile)
+        self.logFilePathButton=QPushButton('Choose .log File Path')
+        self.lableLogFile=QLabel('None')
+        self.chooseLayout.addRow(self.logFilePathButton,self.lableLogFile)
+
+        self.lablePlot=QLabel('Pictrue\'s location')
+        self.centralLayout.addWidget(self.lablePlot)
+
+        self.functionLayout=QHBoxLayout()
+        self.centralLayout.addLayout(self.functionLayout)
+        self.readButton=QPushButton('Read Data')
+        self.functionLayout.addWidget(self.readButton)
+        self.plotButton=QPushButton('plot')
+        self.functionLayout.addWidget(self.plotButton)
+
+
+        self.message=QTextEdit()
+        self.message.append('>>>Program Start')
+        self.centralLayout.addWidget(self.message)
+        self.message.setMaximumHeight(200)
         
-        self.lableDatFile=QtWidgets.QLabel('None')
-        self.lableLogFile=QtWidgets.QLabel('None')
-        self.lableRead=QtWidgets.QLabel('None')
-        self.lablePlot=QtWidgets.QLabel('Pictrue\'s location')
 
-        datFilePathButton.clicked.connect(self.chooseDatFile)
-        logFilePathButton.clicked.connect(self.chooseLogFile)
-        readButton.clicked.connect(self.readData)
-        plotButton.clicked.connect(self.plot)
+        self.datFilePathButton.clicked.connect(self.chooseDatFile)
+        self.logFilePathButton.clicked.connect(self.chooseLogFile)
+        self.readButton.clicked.connect(self.readData)
+        self.plotButton.clicked.connect(self.plot)
 
 
-        vbox1=QtWidgets.QVBoxLayout()
-        hbox=QtWidgets.QHBoxLayout()
-        vbox2=QtWidgets.QVBoxLayout()
 
-
-        vbox1.addWidget(datFilePathButton)
-        vbox1.addWidget(self.lableDatFile)
-        vbox1.addStretch(5)
-        vbox1.addWidget(self.lablePlot)
-        vbox1.addStretch(5)
-        vbox1.addWidget(readButton)
-        vbox1.addWidget(plotButton)
-        vbox1.addWidget(self.lableRead)
-
-
-        vbox2.addWidget(logFilePathButton)
-        vbox2.addWidget(self.lableLogFile)
-        vbox2.addStretch(10)
-
-        hbox.addLayout(vbox1)
-        hbox.addStretch(1)
-        hbox.addLayout(vbox2)
-        hbox.addStretch(10)
-
-        self.setLayout(hbox)
-
-        self.setGeometry(300,300,1000,600)
+        self.setGeometry(200,125,1500,800)
         self.setWindowTitle('Analysis')
         self.show()
 
 
     def chooseDatFile(self):
-        path,ok=QtWidgets.QFileDialog.getOpenFileName(self,)
+        path,ok=QFileDialog.getOpenFileName(self,)
         if ok:
             self.lableDatFile.setText(str(path))
 
     def chooseLogFile(self):
-        path,ok=QtWidgets.QFileDialog.getOpenFileName(self,)
+        path,ok=QFileDialog.getOpenFileName(self,)
         if ok:
             self.lableLogFile.setText(str(path))
 
     def readData(self):
-        if self.lableDatFile.text()=='None' or self.lableLogFile.text()=='None':
+        if self.lableLogFile.text()=='None':
             pass
         else:
-            self.component=hy.CurrentComponent(self.lableDatFile.text(),self.lableLogFile.text())
-            self.lableRead.setText('Read compelete')
+            self.component=cp.CurrentComponent(self.lableDatFile.text(),self.lableLogFile.text())
+            self.message.append('>>>Read compelete')
 
     def plot(self):
-        if self.lableRead.text()=='None':
-            pass
-        else:
-            self.lableRead.setText('Loading Data')
-            self.component.visualData(1,1,1)
-            pic=QtGui.QPixmap('1.png')
-            self.lablePlot.setPixmap(pic)
-            self.lableRead.setText('Plot complete')
+        self.component.visualData(1,1,1)
+        pic=QPixmap('1.png')
+        self.lablePlot.setPixmap(pic)
+        self.message.append('>>>Plot complete')
 
     
 
@@ -91,7 +84,7 @@ class Mywindow(QtWidgets.QWidget):
 
 
 def main():
-    app=QtWidgets.QApplication(sys.argv)
+    app=QApplication(sys.argv)
     mywindow=Mywindow()
     sys.exit(app.exec_())
 
--- WaysToYield.py
+++ WaysToYield.py
@@ -1,8 +1,8 @@
 
 import numpy as np
 
-def AreaMethod(skeleton:dict):
-    '''    Using principle of equal Area to seek yield point (Consume same amount of energy from zero to peak )''' 
+def areaMethod(skeleton:dict):
+    '''Using principle of equal Area to seek yield point (Consume same amount of energy from zero to peak )''' 
     polyCoeff=np.polyfit(skeleton['Disp'],skeleton['Force'],4)
     polyNominal=np.poly1d(polyCoeff)
     ForcePeak=max(skeleton['Force'])
@@ -16,6 +16,44 @@ def AreaMethod(skeleton:dict):
             SwapAreaContrast=AreaContrast
             DispSelectYieldPoint=DispYieldPoint
     return {"Disp":DispSelectYieldPoint,'Force':polyNominal(DispSelectYieldPoint),'ErrorGap':SwapAreaContrast}
+
+def geometry(skeleton:dict):
+    polyCoeff=np.polyfit(skeleton['Disp'],skeleton['Force'],4)
+    polyNominal=np.poly1d(polyCoeff)
+    kInit=polyNominal(1)-polyNominal(0)
+    ForcePeak=max(skeleton['Force'])
+    DispPeak=skeleton['Disp'][skeleton['Force'].index(ForcePeak)]
+    DispSwap=ForcePeak/kInit
+    ForceSwap=polyNominal(DispSwap)
+    DispYieldPoint=DispSwap/ForceSwap*ForcePeak
+    return {'Disp':DispYieldPoint,'Force':polyNominal(DispYieldPoint)}
+
+def rpartMethod(skeleton:dic):
+    polyCoeff=np.polyfit(skeleton['Disp'],skeleton['Force'],4)
+    polyNominal=np.poly1d(polyCoeff)
+    ForcePeak=max(skeleton['Force'])
+    DispPeak=skeleton['Disp'][skeleton['Force'].index(ForcePeak)]
+    ForceSwap=0.75*ForcePeak
+    DispSwap,gap=0,1500
+    for i in np.linspace(0,DispPeak,0.1):
+        if abs(polyNominal(i)-ForceSwap)<gap:
+            gap=abs(polyNominal(i)-ForceSwap)
+            DispSwap=i
+    DispYieldPoint=DispSwap/ForceSwap*ForcePeak
+    return {'Disp':DispYieldPoint,'Force':polyNominal(DispYieldPoint)}
+
+def energyDissipation(skeleton:dict):
+    DispMax,DispMin=max(skeleton['Disp']),min(skeleton['Disp'])
+    IndexMax,IndexMin=skeleton['Disp'].index(DispMax),skeleton['Force'].index(DispMin)
+    if IndexMax<IndexMin:
+        IndexMin,IndexMax=IndexMax,IndexMin
+    oneHalfLoop={"Disp":skeleton['Disp'][IndexMin:IndexMax],'Force':skeleton['Force'][IndexMin,IndexMax]}
+    remainDisp,remainForce=skeleton['Disp'][0:IndexMin],skeleton['Force'][0:IndexMin]
+    remainDisp.extend(skeleton['Disp'][IndexMax:])
+    remainForce.extend(skeleton['Force'][IndexMax:])
+    
+
+
 
 
 
